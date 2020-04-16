7294ffc48c84ebcdd863f6cf76e8998a40f20109
Frederic Horsch <frederichorsch@Frederics-MacBook-Pro.local>
2020-04-16 19:38:44 +0200

    Change Responsive

---
+++ index.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <link rel="stylesheet" href="styles.css">
+    <meta charset="UTF-8">
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+
+    <!-- <script src="https://use.fontawesome.com/939e9dd52c.js"></script> -->
+
+    <!-- Breakpoint -->
+    <meta content="width=device-width, initial-scale=1" name="viewport" />
+
+    <title>HTML Tutorial</title>
+    <script src="script.js"></script>
+
+</head>
+
+<body>
+
+    <!-- First Page of the main page -->
+
+    <div class="flexBox-1 firstPagebox">
+
+        <!-- Webpage intro Homepage -->
+        <!-- 1. Page of Homepage -->
+
+        <div class="firstPageboxLeft">
+            <!-- Logo for the Webpage -->
+
+            <div id="divChimpLogo">
+                <img id="chimpLogo" src="Icons/Chimp_noback.png" alt="Chimpanzee">
+                <a href="">Chimp</a>
+            </div>
+
+            <div id="sloganMainPage">
+
+                <h1>Qick and easy<br />Team managing tool</h1>
+
+            </div>
+
+            <div id="fristBoxDownloadBtn">
+
+                <div class="circleChimp">
+                    <img class="downloadIconMainPage" src="Icons/Download_Icon.png" alt="Download">
+                </div>
+
+                <a class="freeDownloadMainPage" href="">Free Download</a>
+
+            </div>
+        </div>
+        <div class="flexBox-3 firstPageboxRight">
+            <div class="firstPageDIVAnimation">
+                <img id="chimpOnDesk" src="Objects/Chimp_Desk_Laptop.png" alt="Chimpanzee">
+            </div>
+
+        </div>
+    </div>
+
+    <!-- Main Objects for the Webapge -->
+    <!-- Social Media Links -->
+    <!-- Scroller on the right side of the page -->
+    <?php include 'mediaIcons.php'; ?>
+
+    <!-- Second Page of the main page -->
+    <div class="flexBox-2">
+
+        <!-- Left side of the second page -->
+
+        <div class="flexBox-3 leftDIVSecondPage">
+            <!-- <div class="leftSideDIVSecondPage"> -->
+
+            <h2 id="textSecondPageFont36">The fastest way to<br />manag your company</h2>
+
+            <h3 id="textSecondPageFont16">Chimp is a user-friendly APP to structure<br />your company from A to B</h3>
+
+            <div id="secondBoxDownloadBtn">
+
+                <div class="circleChimp">
+                    <img class="downloadIconMainPage" src="Icons/Download_Icon.png" alt="Download">
+                </div>
+
+                <a class="freeDownloadMainPage" href="">Download Now</a>
+
+            </div>
+
+            <!-- </div> -->
+        </div>
+
+        <div class="flexBox-3 rightDIVSecondPage">
+            <div class="rightSideDIVSecondPage">
+
+            </div>
+        </div>
+
+    </div>
+
+    <!-- Third Page with a fucking nice slogan!! -->
+
+    <div class="flexBox-3" id="thirdPageMainDIV">
+
+
+        <h2>Just put something here what can be interesting<br />for our users, like an award we won somewhere </h2>
+
+
+    </div>
+
+    <!-- Fourth Page with a fucking nice slogan!! -->
+
+
+    <div class="fourthPageMainDIV">
+
+        <div class="flexBox-2 DIVBoxBenefitsOfChimp">
+
+            <div class="headingOfPagefour">
+
+                <h2>Benefits of having a Chimp</h2>
+
+            </div>
+
+            <!-- <div class="flexBox-4">
+
+                <div class="blogDivOfBenefits">
+                    <img src="Objects/theKingOfChimps.png" alt="">
+                </div>
+
+                <div class="blogDivOfBenefits">
+                    <img src="Objects/theKingOfChimps.png" alt="">
+                </div>
+
+                <div class="blogDivOfBenefits">
+                    <img src="Objects/theKingOfChimps.png" alt="">
+                </div>
+
+                <div class="blogDivOfBenefits">
+                    <img src="Objects/theKingOfChimps.png" alt="">
+                </div>
+
+                <div class="blogDivOfBenefits">
+                    <img src="Objects/theKingOfChimps.png" alt="">
+                </div>
+
+                <div class="blogDivOfBenefits">
+                    <img src="Objects/theKingOfChimps.png" alt="">
+                </div>
+
+            </div> -->
+
+
+        </div>
+
+    </div>
+
+
+
+    <!-- Fifth Page with a fucking nice slogan!! -->
+
+    <div class="flexBox-2">
+
+        <div class="fifthPageMainDIV">
+
+            <div class="appleEcoSystemSlogan">
+
+                <h2>Chimp is perfectly fitted for the Apple ECO-System<br />You can use it on Mac OS iPhone IOS Watch
+                    OS<br />It works perfectly over all these devices and blabla </h2>
+
+            </div>
+
+            <div class="appleEcoSystemPic">
+                <img src="Objects/Apple_device.jpg" alt="Apple">
+            </div>
+
+        </div>
+
+    </div>
+
+</body>
+
+</html>
---
+++ pricing.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <link rel="stylesheet" href="styles.scss">
+    <link rel="stylesheet" href="pricing.scss">
+    <meta charset="UTF-8">
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+
+    <!-- <script src="https://use.fontawesome.com/939e9dd52c.js"></script> -->
+
+    <!-- Breakpoint -->
+    <meta content="width=device-width, initial-scale=1" name="viewport" />
+
+    <title>HTML Tutorial</title>
+    <script src="script.js"></script>
+
+</head>
+
+<body>
+
+    <!-- Heading bar with menu line -->
+
+    <div class="flexBox-5 navigationBar">
+
+        <div class="flexBox-5 divChimpLogoNavigationBar">
+            <img class="chimpIconNavigationBar" src="Icons/Chimp_noback.png" alt="Chimpanzee">
+            <a href="">Chimp</a>
+        </div>
+
+        <div></div>
+
+        <div class="flexBox-6 divNavigationBarTools">
+            <a href="">Pricing</a>
+            <a href="">Help</a>
+            <a href="">My Account</a>
+            <div class="flexBox-3 downloadNavBar">
+                <p>Download</p>
+            </div>
+        </div>
+
+    </div>
+
+    <!-- Social Media Links -->
+    <!-- Scroller on the right side of the page -->
+    <?php include 'mediaIcons.php';?>
+
+    
+
+
+</body>
+
+</html>
